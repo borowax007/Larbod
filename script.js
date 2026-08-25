c842d4e5cee0ad8e507bc16479bb9b48bda7f393
Ayobode Omobosoye <borowax007@gmail.com>
2026-08-25 06:04:33 -0700

    Improve contact form and navigation

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function contactMessage() {
     alert("Welcome to Larbod. We will contact you soon!");
 }
+const contactForm = document.getElementById("contactForm");
+
+contactForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value.trim();
+const email = document.getElementById("email").value.trim();
+const message = document.getElementById("message").value.trim();
+
+    if (name === "" || email === "" || message === "") {
+        alert("Please fill in all fields.");
+        return;
+    }
+}
+
+    alert("Thank you! Your message is ready to be sent.");
+
+  contactForm.reset();
+
+});
+  
