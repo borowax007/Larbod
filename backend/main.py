2b551ba4862393f5e69a02acfbfb5a92bce15ffb
Ayobode Omobosoye <borowax007@gmail.com>
2026-09-03 08:00:40 -0700

    corsmiddleware update to main.py

--- backend/main.py
+++ backend/main.py
@@ -4,18 +4,23 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
-from pydantic import BaseModel, EmailStr, Field
 
 from routers import contacts
 
-# Project root: Larbod/
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 app = FastAPI(title="Larbod")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(contacts.router)
 
-# Serve the assets folder
 app.mount(
     "/assets",
     StaticFiles(directory=BASE_DIR / "assets"),
@@ -23,19 +28,16 @@ app.mount(
 )
 
 
-# Serve the Larbod website
 @app.get("/")
 def home():
     return FileResponse(BASE_DIR / "index.html")
 
 
-# Serve CSS
 @app.get("/style.css")
 def style():
     return FileResponse(BASE_DIR / "style.css")
 
 
-# Serve JavaScript
 @app.get("/script.js")
 def script():
     return FileResponse(BASE_DIR / "script.js")
